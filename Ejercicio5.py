63f57cd948e615c63746f43755398e9536fd8fcb
yisusdrw <jesus.esquivelc@uanl.edu.mx>
2025-02-04 17:43:31 -0600

    Ejercicio5.py resuelto

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,1 +1,11 @@
-print("aqui viendo como se sube un proyecto del repositorio al github desde visual studio")
+#print("aqui viendo como se sube un proyecto del repositorio al github desde visual studio")
+#OMG FUNCIONOOOOO
+#Profe si vé esto, no le haga caso, el nombre de "a ver aver", era nomás para ver si jalaba y sí se pudo
+
+#Ejercicio 5 - Sabatino 01-02-25, a 04-02-25
+precio = float(input("Precio del producto: "))
+descuento = float(input("Porcentaje del descuento: "))
+k = 100
+decimal = (k-descuento)/100 
+resultado=(decimal*precio)/k
+print(resultado)
