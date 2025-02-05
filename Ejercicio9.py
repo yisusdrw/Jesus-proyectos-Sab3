c23ec4c4bb8f4ce36810eb72265056553963e72a
yisusdrw <jesus.esquivelc@uanl.edu.mx>
2025-02-04 18:49:57 -0600

    listo

---
+++ Ejercicio9.py
@@ -0,0 +1,6 @@
+#Ejercicio 9 - Sabatino 01-02-25, a 04-02-25
+C1 = float(input("Calificacion 1: "))
+C2 = float(input("Calificacion 2: "))
+C3 = float(input("Calificacion 3: "))
+p = (C1+C2+C3)/3
+print("promedio:",p)
