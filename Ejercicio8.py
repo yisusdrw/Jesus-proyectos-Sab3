4fb60d73b38646dc6da2f543c0fc81cfe46b13f8
yisusdrw <jesus.esquivelc@uanl.edu.mx>
2025-02-04 18:43:41 -0600

    listo

---
+++ Ejercicio8.py
@@ -0,0 +1,9 @@
+#Ejercicio 8 - Sabatino 01-02-25, a 04-02-25
+numero_a = int(input("Año: "))
+
+if numero_a%4==0:
+    print("El año SI es biciesto")
+elif numero_a%100:
+    print("El año NO es biciesto")
+
+
