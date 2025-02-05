4d51207cec2182bd81606620914f45900c71d348
yisusdrw <jesus.esquivelc@uanl.edu.mx>
2025-02-04 18:18:49 -0600

    listo

---
+++ Ejercicio6.py
@@ -0,0 +1,6 @@
+#Ejercicio 6 - Sabatino 01-02-25, a 04-02-25
+grados_C = float(input("Grados Celcius: "))
+K = grados_C+273.15
+F = grados_C*1.8+32
+print("Conversion en Farenheit:",F)
+print("Conversion en Kelvin:",K)
